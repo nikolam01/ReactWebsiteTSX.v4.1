eba77c0f22168cceb2ff9ebe59ed856d46a8cf90
Nikola Mirilo <nikolamirilo@gmail.com>
2021-07-17 23:12:52 +0200

    fixed navbar error, added animation to navbar

--- src/pages/CV.tsx
+++ src/pages/CV.tsx
@@ -3,12 +3,14 @@ import DocViewer from '../components/DocViewer/DocViewer'
 
 //components
 import Navbar from '../components/Navbar/Navbar'
+import Footer from '../components/Footer/Footer'
 
 const CV: React.FC = () => {
   return (
     <div className="CV">
       <Navbar />
       <DocViewer source="https://file-examples-com.github.io/uploads/2017/02/file-sample_100kB.doc" />
+      <Footer />
     </div>
   )
 }
