cf97abd397d5c4b522c58cf9426bb103f48e37fe
Nikola Mirilo <nikolamirilo@gmail.com>
2021-08-03 11:50:59 +0200

    worked on carousel

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,60 +1,53 @@
 import React from 'react'
-import img from '../../Assets/Images/Projects/calculator.png'
+import img1 from '../../Assets/Images/Projects/calculator.png'
+import img2 from '../../Assets/Images/Projects/web.tsx.png'
+import './Carousel.scss'
 
 const Carousel: React.FC = () => {
   return (
-    <div
-      id="carouselExampleIndicators"
-      className="carousel slide"
-      data-ride="carousel"
-    >
-      <ol className="carousel-indicators">
-        <li
-          data-target="#carouselExampleIndicators"
-          data-slide-to="0"
-          className="active"
-        ></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-      </ol>
-
+    <div className="Carousel">
       <div
-        className="carousel-item active"
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          padding: '5%',
-          backgroundColor: 'black',
-        }}
+        id="carouselExampleControls"
+        className="carousel slide"
+        data-ride="carousel"
       >
-        <img className="d-block w-50" src={img} alt="First slide"></img>
-      </div>
-      <div className="carousel-item">
-        <img className="d-block w-100" src="..." alt="Second slide"></img>
-      </div>
-      <div className="carousel-item">
-        <img className="d-block w-100" src="..." alt="Third slide"></img>
-      </div>
+        <div className="carousel-inner" id="carouselItems">
+          <div className="carousel-item active">
+            <img className="d-block w-100" src={img1} alt="First slide" />
+          </div>
+          <div className="carousel-item">
+            <img className="d-block w-100" src={img2} alt="Second slide" />
+          </div>
+          <div className="carousel-item">
+            <img className="d-block w-100" src={img1} alt="Third slide" />
+          </div>
+        </div>
 
-      <a
-        className="carousel-control-prev"
-        href="#carouselExampleIndicators"
-        role="button"
-        data-slide="prev"
-      >
-        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span className="sr-only">Previous</span>
-      </a>
-      <a
-        className="carousel-control-next"
-        href="#carouselExampleIndicators"
-        role="button"
-        data-slide="next"
-      >
-        <span className="carousel-control-next-icon" aria-hidden="true"></span>
-        <span className="sr-only">Next</span>
-      </a>
+        <a
+          className="carousel-control-prev"
+          href="#carouselExampleControls"
+          role="button"
+          data-slide="prev"
+        >
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="sr-only">Previous</span>
+        </a>
+        <a
+          className="carousel-control-next"
+          href="#carouselExampleControls"
+          role="button"
+          data-slide="next"
+        >
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="sr-only">Next</span>
+        </a>
+      </div>
     </div>
   )
 }
--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Carousel from '../components/Carousel/Carousel'
 
-function Test() {
+const Test = () => {
   return (
     <div>
       <Carousel />
